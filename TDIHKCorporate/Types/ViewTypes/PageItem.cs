5bfe8428c1b2cf86a7004396261c37d676596b56
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2020-11-28 20:27:55 +0300

    last check

---
+++ TDIHKCorporate/Types/ViewTypes/PageItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDIHKCorporate.Types.ViewTypes
+{
+    public class PageItem
+    {
+        public int PageID { get; set; }
+        public string PageIdentifier { get; set; }
+        public string PageTitle { get; set; }
+        public string PageLanguage { get; set; }
+        public string PageSeoLink { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string Creator { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string Updater { get; set; }
+
+    }
+}
