3f1639f0f730e15dfef9dcaf916a65e3047ebbc3
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2020-11-29 05:34:21 +0300

    html editor için file browser ve image browser yazıldı

---
+++ TDIHKCorporate/Areas/Management/Controllers/ImageController.cs
@@ -0,0 +1,276 @@
+﻿using DbAccess.Dapper.Repository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDIHKCorporate.Enums;
+using TDIHKCorporate.Helpers.Extensions;
+using TDIHKCorporate.Types;
+using TDIHKCorporate.Types.ComponentTypes;
+
+namespace TDIHKCorporate.Areas.Management.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        private const string contentFolderRoot = "~/Content/MainSite/assets";
+        private const string prettyName = "images/";
+        private readonly ThumbnailCreator thumbnailCreator;
+        private readonly DirectoryBrowser directoryBrowser;
+
+        private const int ThumbnailHeight = 80;
+        private const int ThumbnailWidth = 80;
+
+        private const string DefaultFilter = "*.png,*.gif,*.jpg,*.jpeg";
+
+        public ImageController()
+        {
+            thumbnailCreator = new ThumbnailCreator();
+            directoryBrowser = new DirectoryBrowser();
+        }
+
+        public string ContentPath
+        {
+            get
+            {
+                return Path.Combine(contentFolderRoot, prettyName);
+            }
+        }
+
+        // GET: Image
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string ImageBrowserRead(string path)
+        {
+            path = NormalizePath(path);
+
+            directoryBrowser.Server = Server;
+
+            var result = directoryBrowser
+                .GetContent(path, DefaultFilter)
+                .Select(f => new
+                {
+                    name = f.Name,
+                    type = f.Type == EntryType.File ? "f" : "d",
+                    size = f.Size
+                });
+
+            return JsonConvert.SerializeObject(result);
+        }
+
+        private string NormalizePath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return ToAbsolute(ContentPath);
+            }
+
+            return CombinePaths(ToAbsolute(ContentPath), path);
+        }
+
+        private string ToAbsolute(string virtualPath)
+        {
+            return VirtualPathUtility.ToAbsolute(virtualPath);
+        }
+
+        private string CombinePaths(string basePath, string relativePath)
+        {
+            return VirtualPathUtility.Combine(VirtualPathUtility.AppendTrailingSlash(basePath), relativePath);
+        }
+        public FileContentResult ThumbnailRead(string path)
+        {
+            path = NormalizePath(path);
+
+            var physicalPath = Server.MapPath(path);
+
+            if (System.IO.File.Exists(physicalPath))
+            {
+                Response.AddFileDependency(physicalPath);
+
+                return CreateThumbnail(physicalPath);
+            }
+            else
+            {
+                return null;
+                //throw new HttpException(404, "File Not Found");
+            }
+
+        }
+
+        private FileContentResult CreateThumbnail(string physicalPath)
+        {
+            using (var fileStream = System.IO.File.OpenRead(physicalPath))
+            {
+                var desiredSize = new ImageSize
+                {
+                    Width = ThumbnailWidth,
+                    Height = ThumbnailHeight
+                };
+
+                const string contentType = "image/png";
+
+                return File(thumbnailCreator.Create(fileStream, desiredSize, contentType), contentType);
+            }
+        }
+
+        protected virtual bool CanAccess(string path)
+        {
+            return path.StartsWith(ToAbsolute(ContentPath), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public virtual bool AuthorizeCreateDirectory(string path, string name)
+        {
+            return CanAccess(path);
+        }
+
+        [HttpPost]
+        public virtual ActionResult ImageBrowserCreate(string path, FileBrowserEntry entry)
+        {
+            path = NormalizePath(path);
+            var name = entry.Name;
+
+            if (!string.IsNullOrEmpty(name) && AuthorizeCreateDirectory(path, name))
+            {
+                var physicalPath = Path.Combine(Server.MapPath(path), name);
+
+                if (!Directory.Exists(physicalPath))
+                {
+                    Directory.CreateDirectory(physicalPath);
+                }
+
+                return Json(null);
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        [HttpPost]
+        public virtual ActionResult ImageBrowserDestroy(string path, string name, string type)
+        {
+            path = NormalizePath(path);
+
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(type))
+            {
+                path = CombinePaths(path, name);
+                if (type.ToLowerInvariant() == "f")
+                {
+                    DeleteFile(path);
+                }
+                else
+                {
+                    DeleteDirectory(path);
+                }
+
+                return Json(null);
+            }
+            throw new HttpException(404, "File Not Found");
+        }
+
+        public virtual bool AuthorizeDeleteFile(string path)
+        {
+            return CanAccess(path);
+        }
+
+        public virtual bool AuthorizeDeleteDirectory(string path)
+        {
+            return CanAccess(path);
+        }
+
+        protected virtual void DeleteFile(string path)
+        {
+            if (!AuthorizeDeleteFile(path))
+            {
+                throw new HttpException(403, "Forbidden");
+            }
+
+            var physicalPath = Server.MapPath(path);
+
+            if (System.IO.File.Exists(physicalPath))
+            {
+                System.IO.File.Delete(physicalPath);
+            }
+        }
+
+        protected virtual void DeleteDirectory(string path)
+        {
+            if (!AuthorizeDeleteDirectory(path))
+            {
+                throw new HttpException(403, "Forbidden");
+            }
+
+            var physicalPath = Server.MapPath(path);
+
+            if (Directory.Exists(physicalPath))
+            {
+                Directory.Delete(physicalPath, true);
+            }
+        }
+
+        [HttpPost]
+        public virtual ActionResult ImageBrowserUpload(string path, HttpPostedFileBase file)
+        {
+            path = NormalizePath(path);
+            var fileName = Path.GetFileName(file.FileName);
+
+            if (AuthorizeUpload(path, file))
+            {
+                file.SaveAs(Path.Combine(Server.MapPath(path), fileName));
+
+                return Json(new
+                {
+                    size = file.ContentLength,
+                    name = fileName,
+                    type = "f"
+                }, "text/plain");
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        public virtual bool AuthorizeUpload(string path, HttpPostedFileBase file)
+        {
+            return CanAccess(path) && IsValidFile(file.FileName);
+        }
+
+        private bool IsValidFile(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            var allowedExtensions = DefaultFilter.Split(',');
+
+            return allowedExtensions.Any(e => e.EndsWith(extension, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public ActionResult Image(string path)
+        {
+            path = NormalizePath(path);
+
+            if (AuthorizeImage(path))
+            {
+                var physicalPath = Server.MapPath(path);
+
+                if (System.IO.File.Exists(physicalPath))
+                {
+                    const string contentType = "image/png";
+                    return File(System.IO.File.OpenRead(physicalPath), contentType);
+                }
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        public virtual bool AuthorizeImage(string path)
+        {
+            return CanAccess(path) && IsValidFile(Path.GetExtension(path));
+        }
+
+
+
+    }
+}
---
+++ TDIHKCorporate/Types/Images.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDIHKCorporate.Types
+{
+    public class Images
+    {
+        public int ImageID { get; set; }
+        public string ImagePath { get; set; }
+        public string ImageAlternateText { get; set; }
+        public string ImageFileName { get; set; }
+        public string ImageExtension { get; set; }
+        public string ImageSeoLink { get; set; }
+        public int FileSize { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+
+    }
+}
