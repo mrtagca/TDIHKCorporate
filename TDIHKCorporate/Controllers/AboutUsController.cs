f78833ef77c3904134f8cf81233d043067f3ee99
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2020-11-29 18:30:41 +0300

    part-2 sayfalarının controller ve view leri eklendi.

--- TDIHKCorporate/Controllers/AboutUsController.cs
+++ TDIHKCorporate/Controllers/AboutUsController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TDIHKCorporate.BaseControllers.MultiLanguage;
 
 namespace TDIHKCorporate.Controllers
 {
-    public class AboutUsController : Controller
+    public class AboutUsController : SiteBaseController
     {
         // GET: AboutUs
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Institution()
         {
             return View();
         }
---
+++ TDIHKCorporate/Controllers/ServicesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDIHKCorporate.BaseControllers.MultiLanguage;
+
+namespace TDIHKCorporate.Controllers
+{
+    public class ServicesController : SiteBaseController
+    {
+        // GET: Services
+        public ActionResult Dienstleistungen()
+        {
+            return View();
+        }
+
+        public ActionResult Markteintritt()
+        {
+            return View();
+        }
+
+        public ActionResult Türkei()
+        {
+            return View();
+        }
+
+        public ActionResult Deutschland()
+        {
+            return View();
+        }
+
+        public ActionResult Mustervertrage()
+        {
+            return View();
+        }
+
+        public ActionResult ForderungAndFinanzierung()
+        {
+            return View();
+        }
+
+        public ActionResult Aubenhandelsportal()
+        {
+            return View();
+        }
+
+        public ActionResult TdIhkPortal()
+        {
+            return View();
+        }
+
+        public ActionResult Tobb2b()
+        {
+            return View();
+        }
+
+        public ActionResult Konferenzraum()
+        {
+            return View();
+        }
+
+        public ActionResult Jobangebote()
+        {
+            return View();
+        }
+    }
+}
