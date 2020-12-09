530a8b80b84f650a3f501a24756623c40c708461
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2020-12-10 02:27:49 +0300

    editör güncellemeleri

---
+++ TDIHKCorporate/Types/NewsCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDIHKCorporate.Types
+{
+    public class NewsCategory
+    {
+        public int ID { get; set; }
+        public string Language { get; set; }
+        public string NewsCategoryName { get; set; }
+        public string ColorCode { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
