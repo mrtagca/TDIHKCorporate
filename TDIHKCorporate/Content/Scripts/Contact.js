452284795bc5b7ba5ddaffa50350822912ae0178
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2021-01-04 23:37:04 +0300

    iletişim sayfasındaki form kodlandı.

---
+++ TDIHKCorporate/Content/Scripts/Contact.js
@@ -0,0 +1,54 @@
+﻿function AddContact() {
+
+    var callParams = {
+        endPoint: "/AboutUs/AddContact",
+        requestType: "POST"
+    }
+
+
+    var privacyPolicyCheck = document.getElementById("PrivacyPolicyCheck");
+
+
+    if (privacyPolicyCheck.checked === false) {
+        alert("Please check privacy policy!");
+        return;
+    }
+
+    var name = document.getElementById("name");
+    var surname = document.getElementById("surname");
+    var email = document.getElementById("EmailAdress");
+    var message = document.getElementById("Message");
+
+    var dataParams = {}
+    dataParams.Name = name.value;
+    dataParams.Surname = surname.value;
+    dataParams.Message = message.value;
+
+    if (dataParams.Name === '' || typeof (dataParams.Name) === 'undefined' || dataParams.Name === null) {
+        alert("Name cannot be null!");
+        return;
+    }
+
+    if (dataParams.Surname === '' || typeof (dataParams.Surname) === 'undefined' || dataParams.Surname === null) {
+        alert("Surname cannot be null!");
+        return;
+    }
+
+    if (dataParams.Message === '' || typeof (dataParams.Message) === 'undefined' || dataParams.Message === null) {
+        alert("Message cannot be null!");
+        return;
+    }
+
+    RequestAjax(callParams, dataParams, function (response) {
+
+        response = JSON.parse(response);
+
+        if (response === true) {
+            alert("Your message is sent!");
+            location.reload();
+        }
+        else {
+            alert("Fail!");
+        }
+    });
+}
---
+++ TDIHKCorporate/Types/ContactForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDIHKCorporate.Types
+{
+    public class ContactForm
+    {
+        public int ContactFormID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
