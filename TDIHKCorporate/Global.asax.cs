19396431947d0e190d7de6e2008893acd6349033
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2021-02-05 02:02:17 +0300

    optimizasyon pageinsight

--- TDIHKCorporate/Global.asax.cs
+++ TDIHKCorporate/Global.asax.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -11,6 +12,14 @@ namespace TDIHKCorporate
     {
         protected void Application_Start()
         {
+
+            int workerThreads, ioThreads;
+            ThreadPool.GetMaxThreads(out workerThreads, out ioThreads);
+            ThreadPool.SetMaxThreads(Environment.ProcessorCount, ioThreads);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
